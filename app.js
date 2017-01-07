7fea9784fb3a8f9bfb23f959b7f870f627037833
hanshengzhao1993 <hanshengzhao1993@gmail.com>
2017-01-06 16:09:08 -0800

    Finished React Components

--- app.js
+++ app.js
@@ -1,1 +1,66 @@
-// TODO
+
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      done: false
+    };
+  }
+
+   onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render(){
+
+    var style = {
+      'fontWeight': this.state.done ? 'bold' : 'none'
+    };
+
+    // return (<TodoList todos={['Learn React', 'Crush Recast.ly', 'Maybe sleep']}/>);
+    return (
+      <li style={style} onMouseOver={this.onListItemClick.bind(this)}>{['Learn React', 'Crush Recast.ly', 'Maybe sleep']}</li>
+    ); 
+  }
+
+}
+
+var TodoList = (props) => (
+  <ul>
+    {props.todos.map(todo => {
+      return <li key={todo}> {todo}
+              </li>
+    }
+    )}
+  </ul>
+);
+
+ReactDOM.render(<App/>, document.getElementById('app'));
+
+
+
+
+// var TodoList = (props) => {
+
+//   var onListItemClick = (event) => {
+//     console.log('I got clicked');
+//   };
+//   return (
+//     <ul>
+//       <li onClick={onListItemClick}>{props.todos[0]}</li>
+//       <li>{props.todos[1]}</li>
+//       <li>{props.todos[2]}</li>
+//     </ul>
+//   );
+// }
+
+
+// var App = () => (
+//   <div>
+//     <h2>My Todo List</h2>
+//     <TodoList todos={['Learn React', 'Crush Recast.ly', 'Maybe sleep']}/> // Here we are creating an instance of the `TodoList` component
+//   </div>
+// )
